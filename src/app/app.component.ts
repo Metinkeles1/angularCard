2adbb18a1866b660be528685d9747a9dc78adefa
Metinkeles1 <97405379+Metinkeles1@users.noreply.github.com>
2022-12-18 08:45:44 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularCard';
+
+  posts = [
+    {
+      title: 'Camping',
+      imageUrl: '/assets/camping.jpg',
+      username: 'cbdag',
+      content: 'Kampa gençler'
+    },
+    {
+      title: 'Tırmanış',
+      imageUrl: 'assets/paragliding.jpg',
+      username: 'cbuludag',
+      content: 'Bugünde iyi tırmandım'
+    },
+    {
+      title: 'Surf',
+      imageUrl: 'assets/surf.jpg',
+      username: 'cbdoga',
+      content: 'Hadi biraz surf yapalım'
+    }
+  ];
+
 }
